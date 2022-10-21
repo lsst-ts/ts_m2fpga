b9caf67b32742bcb0295df16f52d2e6fcd6dbd48
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-10-21 13:39:25 -0300

    tests with c code

--- fpgaInterface/NiFpga_mainFPGA.h
+++ fpgaInterface/NiFpga_mainFPGA.h
@@ -24,11 +24,11 @@
 /**
  * The signature of the FPGA bitfile.
  */
-static const char* const NiFpga_mainFPGA_Signature = "7957F9F7BCC5197F4E7942B697839512";
+static const char* const NiFpga_mainFPGA_Signature = "54772BAADCF5E773D0E83F6B11650B9A";
 
 typedef enum
 {
-   NiFpga_mainFPGA_IndicatorBool_dataFifoFull = 0x1801E,
+   NiFpga_mainFPGA_IndicatorBool_dataFifoFull = 0x18012,
 } NiFpga_mainFPGA_IndicatorBool;
 
 typedef enum
@@ -38,19 +38,8 @@ typedef enum
 
 typedef enum
 {
-   NiFpga_mainFPGA_ControlBool_enableCapture = 0x18016,
-   NiFpga_mainFPGA_ControlBool_simulationMode = 0x1801A,
-} NiFpga_mainFPGA_ControlBool;
-
-typedef enum
-{
    NiFpga_mainFPGA_ControlU8_controlRelays = 0x18002,
 } NiFpga_mainFPGA_ControlU8;
-
-typedef enum
-{
-   NiFpga_mainFPGA_ControlU32_loopRate_us = 0x18010,
-} NiFpga_mainFPGA_ControlU32;
 
 typedef enum
 {
---
+++ src/demo.c
@@ -0,0 +1,58 @@
+/*
+ * Example code to C/C++ developer
+ * Using m2-crio-simulator.ls.lsst.org
+ * And C API interface
+ * cRIO-9049
+ */
+
+#include "../fpgaInterface/NiFpga_mainFPGA.h"
+#include <stdio.h>
+#include <stdlib.h>
+//#include <time.h>
+
+int main()
+{
+   /* must be called before any other calls */
+   printf("Initializing...\n");
+   NiFpga_Status status = NiFpga_Initialize();
+   if (NiFpga_IsNotError(status))
+   {
+      NiFpga_Session session;
+      /* opens a session and downloads the bitstream */
+      printf("Opening a session...\n");
+      //NiFpga_MergeStatus(&status);
+      NiFpga_Open(NiFpga_mainFPGA_Bitfile,
+                                        NiFpga_mainFPGA_Signature,
+                                        "RIO0",
+                                        NiFpga_OpenAttribute_NoRun,
+                                        &session);
+      if (NiFpga_IsNotError(status))
+      {
+         /* run the FPGA application */
+         printf("Running the FPGA...\n");
+         NiFpga_Run(session, 0);
+                
+         // add code here //
+
+         printf("Press <Enter> to stop and quit...");
+         getchar();
+         /* stop the FPGA loops */
+         printf("Stopping the FPGA...\n");
+         
+         /* close the session now that we're done */
+         printf("Closing the session...\n");
+         NiFpga_Close(session, 0);
+      }
+      /* must be called after all other calls */
+      printf("Finalizing...\n");
+      NiFpga_Finalize();
+   }
+   /* check if anything went wrong */
+   if (NiFpga_IsError(status))
+   {
+      printf("Error %d!\n", status);
+      printf("Press <Enter> to quit...\n");
+      getchar();
+   }
+   return status;
+}
