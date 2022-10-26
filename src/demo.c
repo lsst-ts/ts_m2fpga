78c433ccb381c3ca86ccf74a6ae6fc175fd1d153
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-10-26 07:00:56 +0000

    Te-Wei's update.

--- src/demo.c
+++ src/demo.c
@@ -19,11 +19,14 @@ int main()
       NiFpga_Session session;
       /* opens a session and downloads the bitstream */
       printf("Opening a session...\n");
-      NiFpga_Open(NiFpga_mainFPGA_Bitfile,
+      
+      NiFpga_Status status = 0;
+      status = NiFpga_Open(NiFpga_mainFPGA_Bitfile,
                                         NiFpga_mainFPGA_Signature,
                                         "RIO0",
                                         NiFpga_OpenAttribute_NoRun,
                                         &session);
+      printf("Status to open the NiFPGA bitfile is %d\n", status);
       
       if (NiFpga_IsNotError(status))
       {
@@ -32,46 +35,29 @@ int main()
          NiFpga_Run(session, 0);
    
          // Variables //
-         uint16_t* data = NULL;
+         uint16_t data[2] = {0, 0};
          size_t numberOfElements;
          uint32_t timeout;
          size_t elementsRemaining = 0; // variable declaration
-         size_t* ptr_elementsRemaining = &elementsRemaining; // pointer declaration
-	 NiFpga_Bool fifoStatus;
+	 NiFpga_Bool fifoStatus = 0;
 
          /* allocate size for the samples to read */
          // data = (uint16_t*) malloc (sizeof (uint16_t));
          
          while(1)
 	 {
-          numberOfElements = 0;
-          timeout = 0;     
-          NiFpga_ReadFifoU16(session, NiFpga_mainFPGA_TargetToHostFifoU16_daqFIFO, 
-                              data, 
+          numberOfElements = 2;
+          timeout = 1000;     
+          status = NiFpga_ReadFifoU16(session, NiFpga_mainFPGA_TargetToHostFifoU16_daqFIFO, 
+                              &data[0], 
                               numberOfElements, 
                               timeout, 
                               &elementsRemaining);
          
-          printf("Elements Remaining = %d\n", *ptr_elementsRemaining);
+          printf("Elements Remaining = %d, status = %d\n", elementsRemaining, status);
+          printf("Data[0] is %d\n", data[0]);
 
-          if (*ptr_elementsRemaining >= 9)
-          {
-             printf("Elements Remaining = %d\n", *ptr_elementsRemaining);
-             numberOfElements = 9;
-             timeout = 0;
-             NiFpga_ReadFifoU16(session, NiFpga_mainFPGA_TargetToHostFifoU16_daqFIFO, 
-                              data, 
-                              numberOfElements, 
-                              timeout, 
-                              &elementsRemaining);
-          }
-
-             // Read the fifoStatus boolean
-	     NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_dataFifoFull, &fifoStatus);
-             if (fifoStatus){printf("fifoStatus is TRUE.\n");}
-		else {printf("fifoStatus is FALSE.\n");}
-
-	     sleep(0.050);
+	  sleep(1);
         }
          printf("Press <Enter> to stop and quit...");
          getchar();
